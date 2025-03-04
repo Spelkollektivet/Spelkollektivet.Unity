49128a224bed6ca885d817ed82f5b9583becf75e
Trikisatan <james.newnorth@inter.ikea.com>
2025-03-04 14:53:50 +0100

    Fix.

---
+++ CBScene.cs
@@ -0,0 +1,54 @@
+#nullable enable
+
+using B         = System.Boolean;
+using S8        = System.SByte;
+using S16       = System.Int16;
+using S32       = System.Int32;
+using S64       = System.Int64;
+using U8        = System.Byte;
+using U16       = System.UInt16;
+using U32       = System.UInt32;
+using U64       = System.UInt64;
+using F32       = System.Single;
+using F64       = System.Double;
+using F128      = System.Decimal;
+using O         = System.Object;
+using T         = System.Type;
+using S         = System.String;
+using Ex        = System.Exception;
+using Math      = System.Math;
+
+using V2F32     = UnityEngine.Vector2;
+using V3F32     = UnityEngine.Vector3;
+using V2S32     = UnityEngine.Vector2Int;
+using V3S32     = UnityEngine.Vector3Int;
+using GO        = UnityEngine.GameObject;
+using Tex2D     = UnityEngine.Texture2D;
+using Scene     = UnityEngine.SceneManagement.Scene;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using Spelkollektivet;
+using Spelkollektivet.Unity;
+
+namespace Spelkollektivet.Unity {
+	[UnityEngine.AddComponentMenu("Spelkollektivet/Scene")]
+	public sealed class CBScene : UnityEngine.MonoBehaviour {
+		/* instance methods */
+
+		private void OnEnable() {
+			if(UnityEngine.Application.isPlaying) {
+				if(GO.Find("/Spelkollektivet") is null) {
+					var _go = new GO("Spelkollektivet");
+
+					_go.AddComponent<CBGame>();
+
+					DontDestroyOnLoad(_go);
+				}
+			}
+		}
+	}
+}
